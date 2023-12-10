ab2d29130c6440675d60ba6f98c0ea21c1d41f22
anhnt256 <anhnt256@gmail.com>
2023-12-10 16:52:41 +0700

    done landing page

---
+++ app/(landing-page)/layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Header } from "./_components/header";
+import { Footer } from "./_components/footer";
+
+const LandingPageLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full bg-slate-100">
+      <Header />
+      <main className="pt-40 pb-20 bg-slate-100">{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default LandingPageLayout;
